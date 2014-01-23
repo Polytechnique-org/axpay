92a49d341973d45ca7e688b0c158d4c56a887fba
Raphaël Barrois <raphael.barrois@polytechnique.org>
2014-01-23 01:13:44 +0100

    Add admin.py

--- axpay/money/admin.py
+++ axpay/money/admin.py
@@ -1,3 +1,57 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014 Polytechnique.org
+# This software is distributed under the GPLv3+ license.
+
+from __future__ import unicode_literals, print_function
+
 from django.contrib import admin
 
+from . import models
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['kind', 'name']
+    search_fields = ['kind', 'name']
+    filter_fields = ['kind']
+
+admin.site.register(models.Service, ServiceAdmin)
+
+
+class ServicePriceAdmin(admin.ModelAdmin):
+    list_display = ['service', 'amount', 'available_since', 'available_until']
+    search_fields = ['service__kind', 'service__name']
+    filter_fields = ['service', 'available_since', 'available_until']
+    date_hierarchy = 'available_since'
+
+admin.site.register(models.ServicePrice, ServicePriceAdmin)
+
+
+class PaymentModeAdmin(admin.ModelAdmin):
+    list_display = ['reference', 'owner', 'kind']
+    search_fields = ['reference', 'owner__first_name', 'owner__last_name']
+    filter_fields = ['kind']
+
+admin.site.register(models.PaymentMode, PaymentModeAdmin)
+
+
+class CashFlowAdmin(admin.ModelAdmin):
+    list_display = ['payment_mode', 'payment_date', 'amount']
+    search_fields = ['payment_mode__owner__first_name', 'payment_mode__owner__last_name']
+    filter_fields = ['payment_mode__kind', 'payment_date']
+    date_hierarchy = 'payment_date'
+
+admin.site.register(models.CashFlow, CashFlowAdmin)
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['service_price', 'cashflow', 'user', 'billing_date']
+    list_filter = ['service_price__service', 'service_price', 'cashflow__payment_mode__kind', 'billing_date']
+    search_fields = [
+        'service_price__service__name', 'service_price__service__kind',
+        'user__first_name', 'user__last_name',
+        'cashflow__payment_mode__owner__first_name', 'cashflow__payment_mode__owner__last_name',
+    ]
+    date_hierarchy = 'billing_date'
+
+admin.site.register(models.Payment, PaymentAdmin)
 
--- axpay/money/models.py
+++ axpay/money/models.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# Copyright (c) 2014 Polytechnique.org
+# This software is distributed under the GPLv3+ license.
 
 from __future__ import unicode_literals, print_function
 
