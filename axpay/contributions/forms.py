Polytechnique-org/axpay

commit a740e48b03bd4c6c0e315936515bc15b06dbc9ed

Move contributor accounts into custom model.

axpay/contributions/forms.py
@@ -5,7 +5,6 @@
 import datetime
 
 from django import forms
-from django.contrib import auth
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _

axpay/sales/forms.py
@@ -5,11 +5,11 @@
 import datetime
 
 from django import forms
-from django.contrib import auth
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
+from axpay.accounts import models as accounts_models
 from axpay.money import models as money_models
 from axpay.money import utils as money_utils
 
@@ -22,7 +22,7 @@ class UserChoiceField(forms.ModelChoiceField):
 class OrderRegisterForm(forms.Form):
     user = UserChoiceField(
         label=_("User"),
-        queryset=auth.get_user_model().objects,
+        queryset=accounts_models.Contributor.objects,
     )
 
     payment_mode_kind = forms.ChoiceField(

axpay/accounts/admin.py
@@ -1,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014 Polytechnique.org
+# This software is distributed under the GPLv3+ license.
+
+
+from django.contrib import admin
+
+from . import models
+
+
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'has_lifetime_contribution',
+        'contributions_payed_until', 'jr_subscribed_until']
+    search_fields = ['first_name', 'last_name']
+    list_filter = ['has_lifetime_contribution', 'contributions_payed_until', 'jr_subscribed_until']
+
+admin.site.register(models.Contributor, ContributorAdmin)

axpay/accounts/models.py
@@ -1,0 +1,75 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014 Polytechnique.org
+# This software is distributed under the GPLv3+ license.
+
+
+from django.core.urlresolvers import reverse
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
+
+
+class ContributorProfileQuerySet(models.QuerySet):
+    def up_to_date(self, at=None):
+        if at is None:
+            at = timezone.now()
+        return (self
+            .filter(
+                models.Q(contributions_payed_until__gte=at.date())
+                | models.Q(has_lifetime_contribution=True)
+            )
+            .distinct()
+        )
+
+    def jr_subscribed(self, at=None):
+        if at is None:
+            at = timezone.now()
+        return self.filter(jr_subscribed_until__gte=at.date())
+
+
+class Contributor(models.Model):
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    email = models.EmailField(_('email address'), blank=True)
+
+    contributions_payed_until = models.DateField(blank=True, null=True, db_index=True,
+        verbose_name=_("contributions payed until"))
+    jr_subscribed_until = models.DateField(blank=True, null=True, db_index=True,
+        verbose_name=_("subscribed to J&R until"))
+    has_lifetime_contribution = models.BooleanField(default=False, db_index=True,
+        verbose_name=_("lifetime contribution"))
+
+    objects = ContributorProfileQuerySet.as_manager()
+
+    class Meta:
+        verbose_name = _('contributor')
+        verbose_name_plural = _('contributors')
+
+    def __str__(self):
+        return self.get_full_name()
+
+    def get_absolute_url(self):
+        return reverse('contributions:contributor-detail', args=[self.pk])
+
+    def get_full_name(self):
+        """
+        Returns the first_name plus the last_name, with a space in between.
+        """
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def up_to_date(self, at):
+        if at is None:
+            at = timezone.now()
+        return (self.has_lifetime_contribution
+            or (self.contributions_payed_until is not None
+                and self.contributions_payed_until >= at.date()
+            )
+        )
+
+    def jr_subscribed(self, at):
+        if at is None:
+            at = timezone.now()
+        return (self.jr_subscribed_until is not None
+                and self.jr_subscribed_until >= at.date())
+

axpay/contributions/views.py
@@ -4,10 +4,10 @@
 
 import collections
 
-from django.contrib.auth import models as auth_models
 from django.core.urlresolvers import reverse
 from django.db import models
 
+from axpay.accounts import models as accounts_models
 from axpay.money import models as money_models
 from axpay.web.views import generic
 
@@ -37,9 +37,7 @@ class ContributorsListView(generic.FilterListView):
     template_name = 'contributions/contributor_list.html'
 
     # List
-    @property
-    def queryset(self):
-        return auth_models.User.objects.filter(ordered_items__isnull=False)
+    model = accounts_models.Contributor
 
     select_related = ['contributor_profile']
 
@@ -68,7 +66,7 @@ class ContributorsListView(generic.FilterListView):
         return users
 
 
-class GroupedItems(object):
+class GroupedItems():
     def __init__(self, items, total):
         self.items = items
         self.total = total
@@ -100,9 +98,7 @@ class ContributorDetailView(generic.DetailView):
     context_object_name = 'contributor'
     template_name = 'contributions/contributor_detail.html'
 
-    @property
-    def queryset(self):
-        return auth_models.User.objects
+    model = accounts_models.Contributor
 
     def get_context_data(self, **kwargs):
         ctxt = super().get_context_data(**kwargs)

axpay/money/admin.py
@@ -8,15 +8,6 @@ from django.contrib import admin
 from . import models
 
 
-class ContributorProfileAdmin(admin.ModelAdmin):
-    list_display = ['contributor', 'has_lifetime_contribution',
-        'contributions_payed_until', 'jr_subscribed_until']
-    search_fields = ['contributor__username', 'contributor__first_name', 'contributor__last_name']
-    list_filter = ['has_lifetime_contribution', 'contributions_payed_until', 'jr_subscribed_until']
-
-admin.site.register(models.ContributorProfile, ContributorProfileAdmin)
-
-
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['kind', 'name']
     search_fields = ['kind', 'name']

axpay/money/models.py
@@ -5,7 +5,6 @@
 
 import datetime
 
-from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.utils import timezone
@@ -14,102 +13,39 @@ from django.utils.translation import ugettext_lazy as _
 from . import utils
 
 
-class ContributorProfileQuerySet(models.QuerySet):
-    def up_to_date(self, at=None):
-        if at is None:
-            at = timezone.now()
-        return (self
-            .filter(
-                models.Q(contributions_payed_until__gte=at.date())
-                | models.Q(has_lifetime_contribution=True)
-            )
-            .distinct()
-        )
-
-    def jr_subscribed(self, at=None):
-        if at is None:
-            at = timezone.now()
-        return self.filter(jr_subscribed_until__gte=at.date())
-
-
-class ContributorProfile(models.Model):
-    """Extra data about a contributor."""
-
-    contributor = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='contributor_profile',
-        verbose_name=_("contributor"))
-
-    contributions_payed_until = models.DateField(blank=True, null=True, db_index=True,
-        verbose_name=_("contributions payed until"))
-    jr_subscribed_until = models.DateField(blank=True, null=True, db_index=True,
-        verbose_name=_("subscribed to J&R until"))
-    has_lifetime_contribution = models.BooleanField(default=False, db_index=True,
-        verbose_name=_("lifetime contribution"))
-
-    objects = ContributorProfileQuerySet.as_manager()
-
-    class Meta:
-        verbose_name = _("contributor profile")
-        verbose_name_plural = _("contributor profiles")
-
-    def __str__(self):
-        return self.contributor.get_full_name()
-
-    def get_absolute_url(self):
-        return reverse('contributions:contributor-detail', args=[self.pk])
-
-    def up_to_date(self, at):
-        if at is None:
-            at = timezone.now()
-        return (self.has_lifetime_contribution
-            or (self.contributions_payed_until is not None
-                and self.contributions_payed_until >= at.date()
-            )
-        )
-
-    def jr_subscribed(self, at):
-        if at is None:
-            at = timezone.now()
-        return (self.jr_subscribed_until is not None
-                and self.jr_subscribed_until >= at.date())
-
-
 def get_expiry_date(billing_date, jr=False):
     if billing_date is None:
         return billing_date
     return datetime.date(year=billing_date.year + 1, month=1, day=1)
 
 
-def recompute_profile(user):
-    try:
-        profile = user.contributor_profile
-    except ContributorProfile.DoesNotExist:
-        profile = ContributorProfile.objects.create(contributor=user)
+def recompute_profile(contributor):
 
     try:
-        latest_contribution_date = (user.ordered_items
+        latest_contribution_date = (contributor.ordered_items
             .filter(product_price__product__kind__in=Product.VALID_CONTRIBUTION_KINDS)
             .latest('billing_date')
         ).billing_date
     except OrderItem.DoesNotExist:
         latest_contribution_date = None
 
-    lifetime_contribution = (user.ordered_items
+    lifetime_contribution = (contributor.ordered_items
         .filter(product_price__product__kind__in=Product.FORLIFE_CONTRIBUTION_KINDS)
         .exists()
     )
 
     try:
-        latest_jr_subscription_date = (user.ordered_items
+        latest_jr_subscription_date = (contributor.ordered_items
             .filter(product_price__product__kind__in=Product.JR_SUBSCRIBED_KINDS)
             .latest('billing_date')
         ).billing_date
     except OrderItem.DoesNotExist:
         latest_jr_subscription_date = None
 
-    profile.contributions_payed_until = get_expiry_date(latest_contribution_date)
-    profile.jr_subscribed_until = get_expiry_date(latest_jr_subscription_date)
-    profile.has_lifetime_contribution = lifetime_contribution
-    profile.save()
+    contributor.contributions_payed_until = get_expiry_date(latest_contribution_date)
+    contributor.jr_subscribed_until = get_expiry_date(latest_jr_subscription_date)
+    contributor.has_lifetime_contribution = lifetime_contribution
+    contributor.save()
 
 
 
@@ -210,7 +146,7 @@ class PaymentMode(models.Model):
         (KIND_DIRECT, _("Direct debit")),
     )
 
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='payment_modes',
+    owner = models.ForeignKey('accounts.Contributor', related_name='payment_modes',
         verbose_name=_("owner"))
     kind = models.CharField(max_length=16, choices=KIND_CHOICES,
         verbose_name=_("kind"))
@@ -258,7 +194,7 @@ class Order(models.Model):
 class OrderItem(models.Model):
     """An item from an order."""
 
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='ordered_items',
+    user = models.ForeignKey('accounts.Contributor', related_name='ordered_items',
         verbose_name=_("user"), help_text=_("user for whom the product was bought"))
     product_price = models.ForeignKey(ProductPrice, related_name='order_items',
         verbose_name=_("product price"))
@@ -296,7 +232,7 @@ class OrderItem(models.Model):
 
 
 def up_to_date(contributor, at=None):
-    return contributor.contributor_profile.up_to_date(at)
+    return contributor.up_to_date(at)
 
 def jr_subscribed(contributor, at=None):
-    return contributor.contributor_profile.jr_subscribed(at)
+    return contributor.jr_subscribed(at)

axpay/money/testing.py
@@ -8,9 +8,9 @@ import datetime
 import factory
 import factory.django
 
-from django.contrib.auth import models as auth_models
 from django.utils import timezone
 
+from axpay.accounts import models as accounts_models
 from . import models
 
 
@@ -65,9 +65,8 @@ class AllPricesFactory(factory.Factory):
 
 class ExamplePaymentOwnerFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model = auth_models.User
+        model = accounts_models.Contributor
 
-    username = factory.Sequence(lambda n: 'demo-owner-%s' % n)
     first_name = "John"
     last_name = factory.Sequence(lambda n: "Demo%s" % n)
 

commit 149217a8cbc48814a9120e7cd678a817d92799f4

No longer use Contributor.username in ContributorFilterForm
Fix bug introduced by commit a740e48b03bd ("Move contributor accounts
into custom model").

axpay/contributions/forms.py
@@ -43,8 +43,7 @@ class ContributorFilterForm(forms.Form):
         if data.get('user_search'):
             lookup = data['user_search']
             qs = qs.filter(
-                models.Q(username__icontains=lookup)
-                | models.Q(first_name__icontains=lookup)
+                models.Q(first_name__icontains=lookup)
                 | models.Q(last_name__icontains=lookup)
             )
 